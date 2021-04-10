b26b420bbb1963879d2fde310522e8cec7d95469
mvinzangelo <mvinzangelo@gmail.com>
2021-04-10 01:44:18 -0700

    FRQ

--- CitrusHack2021/Assets/ScriptableObjects/FRQ.cs
+++ CitrusHack2021/Assets/ScriptableObjects/FRQ.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "FRQ", menuName = "FRQ")]
 public class FRQ : ScriptableObject
 {
     public string question;
