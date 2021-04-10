ba240651840890e8cfef56831452e853fe9305d9
jecndlria <jecndlria@gmail.com>
2021-04-10 16:20:42 -0700

    update flower.cs to have a Grow event.

--- CitrusHack2021/Assets/Scripts/Flower.cs
+++ CitrusHack2021/Assets/Scripts/Flower.cs
@@ -14,6 +14,8 @@ public class Flower : MonoBehaviour
     public int expToGrow;           // how much exp to grow to next phase
     public int wateringInterval;    // how often, in seconds, you need to water the plant
     public bool needsWater;         // if true, you can water the plant for xp.
+    public int randomNumber;
+    public bool grow;               // if true, the plant grew. use this for events involving growth.
 
     public Sprite[] spriteArray;
     public SpriteRenderer spriteRenderer;
@@ -30,8 +32,9 @@ public class Flower : MonoBehaviour
     public int currentProgressToNextLevel() { return progressToNextLevel; }
     public int currentTimeSinceLastWater() { return timeSinceLastWater; }
     public string getFlowerName() { return flowerName; }
-
     public bool needsWatering() { return needsWater; }
+    public bool Grew() { return grow; }
+    public void UpdateGrow() { grow = false; }
 
     public void UpdateSprite()
     {
@@ -46,19 +49,29 @@ public class Flower : MonoBehaviour
             if (growthLevel < 4)
             {
                 growthLevel++;
+                grow = true;
+                Debug.Log("I should only print once.");
             }
         }
     }
+
+
 
     public void WaterTimer()
     {
         timeSinceLastWater++;
     }
+    public int GetRandomNumber() 
+    {
+        randomNumber = Random.Range(1, 100);
+        return randomNumber;  
+    }
 
     private void Start()
     {
         UpdateSprite();
-        InvokeRepeating(nameof(WaterTimer), 1, 1); // will increase timeSinceLastWater by 1 every second
+        InvokeRepeating(nameof(WaterTimer), 1, 1);
+        InvokeRepeating(nameof(GetRandomNumber), 5, 5);
     }
 
     private void Update()
@@ -79,6 +92,7 @@ public class Flower : MonoBehaviour
                     timeSinceLastWater = 0;
                     progressToNextLevel += expPerLevel;
                     checkIfGrow();
+                    grow = false;
                     needsWater = false;
                     Invoke("UpdateSprite", 10.0f);
                 }
