32ce2a6d02a1c5335dbf7df640ed7e3072e052b7
david-niculae <david.niculae@berkeley.edu>
2021-04-11 04:56:15 -0700

    jdf

--- CitrusHack2021/Assets/Scripts/TouchCalendar.cs
+++ CitrusHack2021/Assets/Scripts/TouchCalendar.cs
@@ -15,22 +15,26 @@ public class TouchCalendar : MonoBehaviour
 
             Panel.SetActive(!isActive);        }
     }
+
+    string data1 = "Yes";
     void Update()
     {
 
-       // if (Input.GetMouseButtonDown(0))
-            //openPanel();
-
-
-
-       if (Input.touchCount > .2                                                                                      )
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            if (GetComponent<Collider2D>().OverlapPoint(wp))
-            {
-                openPanel();
-                Debug.Log("Hello");
-            }
+            openPanel();
+            Debug.Log("Hello");
         }
+        /*if (Input.touchCount > .2                                                                                      )
+         {
+             Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+             if (GetComponent<Collider2D>().OverlapPoint(wp))
+             {
+                 openPanel();
+                 Debug.Log("Hello");
+
+
+             }
+         }*/
     }
 }
