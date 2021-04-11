59c31ed41b9d3b5f78d39e5127c559bd03a17caf
mvinzangelo <mvinzangelo@gmail.com>
2021-04-10 17:33:33 -0700

    Data Structures

--- CitrusHack2021/Assets/ScriptableObjects/Days.cs
+++ CitrusHack2021/Assets/ScriptableObjects/Days.cs
@@ -3,12 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Days", menuName = "Days")]
-public class Days
+public class Days : ScriptableObject
 {
+    public void Awake()
+    {
+        setDate();
+    }
+
     public string date;
-    public Question[] answers;
+    public List<Question> answers;
     public int physicalIndex;
     public int mentalIndex;
     public int totalIndex;
     public FRQ FRQ;
+
+    public void push_back_question(Question userQuestion)
+    {
+        Question userAnswer = userQuestion;
+        answers.Add(userAnswer);
+        if (userQuestion.typeOfQuestion)
+        {
+            mentalIndex++;
+        }
+        else
+        {
+            physicalIndex++;
+        }
+        totalIndex++;
+    }
+
+    public void push_back_FRQ(FRQ userFRQ)
+    {
+        FRQ = userFRQ;
+    }
+
+    public void setDate()
+    {
+        date = System.DateTime.Now.ToString("MM/dd");
+    }
 }
