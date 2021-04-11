237f30aa1863df33a88707e70bd5ff0cce5a9b53
david-niculae <david.niculae@berkeley.edu>
2021-04-11 03:41:31 -0700

    calendar buttons

---
+++ CitrusHack2021/Assets/Scripts/TouchCalendar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCalendar : MonoBehaviour
+{
+    public GameObject Panel;
+
+
+    public void openPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);        }
+    }
+    void Update()
+    {
+
+       // if (Input.GetMouseButtonDown(0))
+            //openPanel();
+
+
+
+       if (Input.touchCount > .2)
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            if (GetComponent<Collider2D>().OverlapPoint(wp))
+            {
+                openPanel();
+                Debug.Log("Hello");
+            }
+        }
+    }
+}
